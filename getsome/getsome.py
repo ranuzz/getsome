d6667f6f22b473704d0cd7c2c1d206200ebfd0d7
ranuzz <ranuzz@outlook.com>
2022-01-08 22:08:31 -0500

    let the basic command open a random link in browser

--- getsome/getsome.py
+++ getsome/getsome.py
@@ -1,8 +1,11 @@
 """Main module."""
 
 import feedparser
+from random import choice
+import webbrowser
 
 def readFeeds(lc):
+    all_links = []
     with open('feeds.txt', 'r') as fp:
         for rssfeedlink in fp.readlines():
             feeds = feedparser.parse(rssfeedlink)
@@ -11,10 +14,11 @@ def readFeeds(lc):
             for entry in feeds['entries']:
                 if lc == -1 or c < lc:
                     print(entry['title'], entry['link'])
+                    all_links.append(entry['link'])
                 else:
                     break
                 c += 1
+    webbrowser.open_new_tab(choice(all_links))
 
 def getsome(lc):
     readFeeds(lc)
-
