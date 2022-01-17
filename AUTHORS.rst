00559389756496d7b4bf7f7833509517b2aa0a13
ranuzz <ranuzz@outlook.com>
2022-01-17 11:34:38 -0800

    adding bloat from cookicutter for pypi packaging

---
+++ AUTHORS.rst
@@ -0,0 +1,13 @@
+=======
+Credits
+=======
+
+Development Lead
+----------------
+
+* shekhar chandra <ranuzz@outlook.com>
+
+Contributors
+------------
+
+None yet. Why not be the first?
---
+++ tests/test_getsome.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+
+"""Tests for `getsome` package."""
+
+def test_dummy():
+    """Dummy test to keep something in this folder """
+    result = 0
+    assert result == 0
